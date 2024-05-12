f795539ae94d5ee6ccf587440ae4a6507ca680e1
sasumi <cnsasumi@gmail.com>
2024-05-12 20:15:05 +0800

    upd

--- script/scaffold.php
+++ script/scaffold.php
@@ -2,8 +2,6 @@
 
 use function LFPhp\Func\get_all_opt;
 use function LFPhp\Func\get_screen_size;
-use function LFPhp\Func\mkdir_batch;
-use function LFPhp\Pls\get_pls_config;
 
 include dirname(__DIR__).'/vendor/autoload.php';
 
@@ -13,6 +11,10 @@ const ARG_OPTIONAL = 2;
 $PLS_DOC = <<<EOT
 Plite project scaffold.
 command pattern: php {$_SERVER['SCRIPT_NAME']} [command] [...params] 
+global parameters:
+ -h show help
+ -v show verbose
+ -y confirm operation as default [yes]
 EOT;
 
 const SUPPORT_COMMAND_DOC = [
@@ -28,6 +30,11 @@ const SUPPORT_COMMAND_DOC = [
 		'update'  => 'Update all ORM model and table object from database configs, regenerate all table model but skip exist models.',
 		'--table' => 'Specify tables, use comma [,] to separate more than two tables.',
 	],
+	'crud' => [
+		'generate CRUD file',
+		'--table' => '',
+	],
+	'',
 ];
 
 //start logic
@@ -46,18 +53,10 @@ if(!SUPPORT_COMMAND_DOC[$command]){
 	exit;
 }
 
-switch($command){
-	case 'init':
-		$project_dir = PLS_ROOT.'/test/prj1';
-		$file_struct = get_pls_config('file/file_structure');
-		$file_struct = str_replace('$PROJECT_DIR', $project_dir, $file_struct);
-		mkdir_batch($file_struct);
-		echo 'Project file structure created:',PHP_EOL;
-		foreach($file_struct as $f){
-			echo realpath($f), PHP_EOL;
-		}
-		echo "success";
-		exit;
-}
+$arg_str = join(' ', $options);
+$CMD_SCRIPT = realpath(__DIR__.'/'.$command.'.php');
+$call = "php $CMD_SCRIPT $arg_str";
 
-
+echo "Exec Command: ", $command, PHP_EOL;
+$ret = shell_exec($call);
+echo $ret;
--- src/ProjectInfo.php
+++ src/ProjectInfo.php
@@ -4,5 +4,4 @@ class ProjectInfo {
 	public $project_name;
 	public $project_dir;
 	public $project_namespace;
-	
 }
