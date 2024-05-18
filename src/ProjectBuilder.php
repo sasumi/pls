52e6382cb654cbdda3b381d86d82db5aca94b78c
sasumi <cnsasumi@gmail.com>
2024-05-18 19:11:13 +0800

    upd

--- src/ProjectBuilder.php
+++ src/ProjectBuilder.php
@@ -2,45 +2,119 @@
 
 namespace LFPhp\Pls;
 
+use Composer\InstalledVersions;
 use LFPhp\Logger\Logger;
+use LFPhp\Logger\LoggerLevel;
+use LFPhp\Logger\Output\ConsoleOutput;
+use function LFPhp\Func\console_color;
+use function LFPhp\Func\glob_recursive;
+use function LFPhp\Func\is_assoc_array;
+use function LFPhp\Func\readline;
+use function LFPhp\Func\run_command;
 use function LFPhp\PLite\get_app_name;
 use function LFPhp\PLite\get_app_namespace;
 
+include_once dirname(__DIR__).'/vendor/autoload.php';
+
 class ProjectBuilder {
+	const TEMPLATE_PROJECT = __DIR__.'/../template';
+
+	private static $steps = [
+		'help'              => ['Command Helper'],
+		'init'              => ['Init Project'],
+		'projectEnvConfirm' => ['Project Configuration Confirm'],
+		'initDir'           => ['Initialize project directories'],
+		'initFile'          => ['Initialize project base file'],
+		'initDatabase'      => ['Configuration Database'],
+		'generateORM'       => ['Generate ORM'],
+		'generateIndexPage' => ['Generate Index'],
+	];
+
+	public static function start(){
+		Logger::registerGlobal(new ConsoleOutput(), LoggerLevel::DEBUG);
+		$args = array_slice($_SERVER['argv'], 2);
+		Logger::debug('Arguments detected: ', $args);
+		$command = array_shift($args);
+		$support_commands = array_keys(self::$steps);
+		if(!in_array($command, $support_commands)){
+			self::help();
+		}
+		call_user_func([self::class, $command]);
+	}
+
+	public static function help(){
+		$commands = [];
+		foreach(self::$steps as $method => list($title)){
+			$commands[] = console_color($method, 'brown')." \t\t\t\t".console_color($title, 'light_gray');
+		}
+		$helper_msg = "\n".console_color('PLite Project Builder', 'white', 'yellow')."\n"
+			.console_color('[Usage]', 'green')."\n".
+			"composer pls <command> [...arguments]\n\n".
+			console_color('[Commands Support]', 'green')."\n".
+			join("\n", $commands)."\n";
+		die($helper_msg);
+	}
+
 	public static function init(){
+		Logger::info('==== Start Init Project ====');
+
 		$steps = [
-			'Project Configuration Confirm'  => [ProjectBuilder::class, 'projectEnvConfirm'],
-			'Initialize project directories' => [ProjectBuilder::class, 'MakeDir'],
-			'Initialize project base file'   => [ProjectBuilder::class, 'initFile'],
-			'Configuration Database'         => [ProjectBuilder::class, 'initDatabase'],
-
+			'projectEnvConfirm',
+			'initDir',
+			'initFile',
+			'initDatabase',
+			'generateORM',
+			'generateIndexPage',
 		];
-		Logger::warning('==== Start Init Project ==== ');
-
+		$step_counter = 1;
 		$total_steps = count($steps);
-		$step_index = 0;
-		foreach($steps as $name => $payload){
-			Logger::info("Step [$step_index/$total_steps]: ", $name);
-			call_user_func($payload);
+		foreach($steps as $method){
+			$title = self::$steps[$method][0];
+			Logger::info("Step[$step_counter/$total_steps] $title");
+			call_user_func([self::class, $method]);
+			$step_counter++;
 		}
 		Logger::info('---- Project Init Done ----');
 	}
 
 	private static function projectEnvConfirm(){
-		$app_name = get_app_name();
-		$namespace = get_app_namespace();
+		$envs = self::getProjectInfo();
+		$envs_str = [];
+		foreach($envs as $k => $v){
+			$ks = ucwords(str_replace(['-', '_'], [' ', ' '], $k));
+			$ks = ucfirst($ks);
+			$envs_str[] = "$ks: \t$v";
+		}
+		$envs_str = join(PHP_EOL, $envs_str);
 		$info = <<<EOT
-Project name detected: $app_name
-Project namespace to be used: $namespace 
+
+------------------------------
+$envs_str
+------------------------------ 
 EOT;
 		Logger::info($info);
+		if(!console_confirm("Is that above information correct?")){
+			die('Exit');
+		}
 	}
 
-	/**
-	 * 初始化必要的文件
-	 * @return void
-	 */
-	private static function initFile(){
+	public static function getProjectInfo(){
+		$r = InstalledVersions::getRootPackage();
+		$app_root = realpath($r['install_path']);
+		$app_name = get_app_name();
+		$app_namespace = get_app_namespace();
+//
+//		$app_root = dirname(__DIR__).'/test/hello';
+//		$app_name = 'swing/hello';
+//		$app_namespace = 'Swing\\Hello';
+
+		$app_name_var = str_replace('\\', '', $app_namespace);
+		return [
+			'app_root'      => realpath($app_root),
+			'app_name'      => $app_name,
+			'app_name_var'  => $app_name_var,
+			'app_namespace' => $app_namespace,
+		];
 	}
 
 	/**
@@ -48,17 +122,45 @@ EOT;
 	 * @return void
 	 * @throws \Exception
 	 */
-	private static function MakeDir(){
-		$file_structs = get_pls_config('file/file_structure');
+	public static function initDir(){
+		$file_structs = self::getTemplateProjectStructure();
 		foreach($file_structs as $dir){
 			if(!is_dir($dir)){
 				if(!mkdir($dir, 0x777, true)){
 					throw new \Exception('Make directory fail:'.$dir);
 				}
-				Logger::info('Directory created:', $dir);
+				Logger::info('Directory created:', realpath($dir));
 			}else{
-				Logger::warning('Directory already exists.', $dir);
+				Logger::debug('Directory already exists.', realpath($dir));
 			}
+		}
+	}
+
+	/**
+	 * 初始化必要的文件
+	 * @return void
+	 */
+	public static function initFile(){
+		$overwrite = false;
+		$file_map = self::getTemplateProjectFiles();
+		foreach($file_map as $src_file => [$target_file, $is_tpl]){
+			if(!$overwrite && is_file($target_file)){
+				Logger::debug('Target file exists: '.$target_file);
+				continue;
+			}
+			Logger::info('Build File:', $target_file);
+			if($is_tpl){
+				$ctn = file_get_contents($src_file);
+				if(strlen($ctn)){
+					$hit = false;
+					$ctn = self::mixingProjectInfo($ctn, $hit);
+					if($hit){
+						file_put_contents($target_file, $ctn);
+						continue;
+					}
+				}
+			}
+			copy($src_file, $target_file);
 		}
 	}
 
@@ -66,6 +168,143 @@ EOT;
 	 * 初始化数据库配置
 	 * @return void
 	 */
-	private static function initDatabase(){
+	public static function initDatabase(){
+		$project_info = self::getProjectInfo();
+		$app_name_var = $project_info['app_name_var'];
+		$app_root = $project_info['app_root'];
+		$database_file = $app_root.'/config/database.inc.php';
+		$config = [];
+		if(is_file($database_file)){
+			try{
+				$config = include $database_file;
+				Logger::debug('Database config file already exists: '.$database_file, $config);
+				if(isset($config[$app_name_var])){
+					Logger::debug('Database config item already exists: ', $config[$app_name_var]);
+					return;
+				}
+			}catch(\Exception $e){
+				Logger::error('Database config file no readable: '.$e->getMessage());
+				Logger::exception($e);
+				return;
+			}
+		}
+		if(!console_confirm('Setup database config now?')){
+			return;
+		}
+		$host = console_read_required('Enter database host: ', true);
+		$database = console_read_required('Enter which database to use: ', true);
+
+		$user = readline('Enter the user for database: ');
+		$user = trim($user);
+		$password = readline('Enter the password for database: ');
+
+		if(!is_file($database_file)){
+			Logger::info('Create database config file: '.$database_file);
+			touch($database_file);
+		}
+
+		$config[$app_name_var] = [
+			'host'     => $host,
+			'user'     => $user,
+			'password' => $password,
+			'database' => $database,
+		];
+		write_php_config_file($database_file, $config);
+		Logger::info('Database config saved: '.$database_file);
+	}
+
+	/**
+	 * 使用项目环境变量替换字符串中的变量
+	 * @param string $str
+	 * @param bool $hit
+	 * @return string
+	 */
+	private static function mixingProjectInfo($str, &$hit = false){
+		$project_info = self::getProjectInfo();
+		foreach($project_info as $k => $v){
+			$c = 0;
+			$str = str_replace('{'.$k.'}', $v, $str, $c);
+			if($c){
+				$hit = true;
+			}
+		}
+		return $str;
+	}
+
+	public static function generateORM(){
+		$project_info = self::getProjectInfo();
+		$app_name_var = $project_info['app_name_var'];
+		$app_root = $project_info['app_root'];
+		$database_file = $app_root.'/config/database.inc.php';
+		if(!is_file($database_file)){
+			Logger::warning('No database config file detected: '.$database_file);
+			return;
+		}
+		try{
+			$config = include $database_file;
+			if(empty($config)){
+				Logger::warning('Empty database config in file: '.$database_file, $config);
+				return;
+			}
+			if(!is_assoc_array($config) || count($config) == count($config, COUNT_RECURSIVE)){
+				Logger::error('Wrong config format in file, two dimension in assoc array required. '.$database_file);
+				return;
+			}
+
+			if(!console_confirm('Start to generate ORM files?')){
+				return;
+			}
+
+			//todo
+			$source_ids = array_keys($config);
+			$source_id = '';
+			if(count($source_ids) > 1){
+				$specs = readline('You have more than one database config, specify which db to generate, default for all DB');
+				if($specs){
+					$source_id = $specs;
+				}
+			}
+			$source_id = $source_id ?: $source_ids[0];
+			$cmd = 'php '.realpath($app_root.'/script/orm/generate.php')." --source=$source_id";
+			Logger::info('Start execute command: '.$cmd);
+			run_command($cmd, [], true);
+			return;
+		}catch(\Exception $e){
+			LOgger::error('Error while reading database config file: '.$database_file);
+			Logger::exception($e);
+		}
+	}
+
+	public static function generateIndexPage(){
+	}
+
+	/**
+	 * @return array
+	 */
+	private static function getTemplateProjectStructure(){
+		$dirs = glob_recursive(self::TEMPLATE_PROJECT.'/*', GLOB_ONLYDIR);
+		$tmpl_dir = realpath(self::TEMPLATE_PROJECT);
+		foreach($dirs as $k => $dir){
+			$dirs[$k] = self::mixingProjectInfo(str_replace($tmpl_dir, '{app_root}', realpath($dir)));
+		}
+		return $dirs;
+	}
+
+	private static function getTemplateProjectFiles(){
+		$dirs = glob_recursive(self::TEMPLATE_PROJECT.'/*');
+		$tmpl_dir = realpath(self::TEMPLATE_PROJECT);
+		$file_map = [//src_file => target_file
+		];
+		foreach($dirs as $k => $file){
+			if(!is_file($file)){
+				continue;
+			}
+			$is_template = !!preg_match('/\.tpl$/', $file);
+			$src_file = realpath($file);
+			$target_file = self::mixingProjectInfo(str_replace($tmpl_dir, '{app_root}', realpath($file)));
+			$target_file = preg_replace('/\.tpl$/', '', $target_file);
+			$file_map[$src_file] = [$target_file, $is_template];
+		}
+		return $file_map;
 	}
 }
--- src/function.php
+++ src/function.php
@@ -1,71 +1,45 @@
 <?php
 namespace LFPhp\Pls;
 
-use Exception;
-use function LFPhp\Func\array_fetch_by_path;
+use function LFPhp\Func\console_color;
 use function LFPhp\Func\readline;
-
-define('PLS_ROOT', dirname(__DIR__));
+use function LFPhp\Func\var_export_min;
 
 /**
- * @param string $template_name
- * @param array $params
+ * white php config file (with return statement in file)
+ * @param $file
+ * @param $config
+ * @return void
+ */
+function write_php_config_file($file, $config){
+	$cfg_arr_str = var_export_min($config, true);
+	$str = <<<EOT
+<?php 
+return $cfg_arr_str;
+EOT;
+	file_put_contents($file, $str);
+}
+
+function console_confirm($confirm_msg = ''){
+	$confirm_msg = $confirm_msg."\nType [y] or [yes] to confirm: ";
+	$input = readline(console_color($confirm_msg, 'yellow'));
+	$input = trim(strtolower($input));
+	return in_array($input, ['y', 'yes']);
+}
+
+/**
+ * @param $msg
+ * @param $trim
  * @return string
- * @throws \Exception
  */
-function mixing_template($template_name, $params = []){
-	$template_file = PLS_ROOT.'/config/template/'.$template_name.'.template';
-	if(!is_file($template_file)){
-		throw new Exception('Template file no found:'.$template_name);
-	}
-	$str = file_get_contents($template_file);
-	foreach($params as $k => $p){
-		$str = str_replace("${$k}", $p, $str);
-	}
-	return $str;
-}
-
-function project_path($uri_pattern){
-	return str_replace('$PROJECT_DIR', $project_dir, $uri_pattern);
-}
-
-
-/**
- * @param string $confirm_msg
- */
-function console_confirm_to_continue($confirm_msg = ''){
-	$retry_count = 3;
-	$confirm_msg = $confirm_msg ?: "Please type [y] or [yes] to continue:";
+function console_read_required($msg, $trim = false){
 	while(true){
-		$input = readline($confirm_msg);
-		if(trim($input) === 'y' || trim($input) === 'yes'){
-			return;
+		$input = readline(console_color($msg, 'yellow'));
+		if($trim){
+			$input = trim($input);
 		}
-		if($retry_count-- == 0){
-			die('Program exit on no response correctly.');
+		if($input){
+			return $input;
 		}
 	}
-}
-
-
-/**
- * get pls config
- * @param $name
- * @return array|mixed
- * @throws \Exception
- */
-function get_pls_config($name){
-	$args = explode('/', $name);
-	$file = array_shift($args);
-
-	$config_file = PLS_ROOT.'/config/'.$file.'.inc.php';
-	if(!is_file($config_file)){
-		throw new Exception('Config file no found:'.$config_file);
-	}
-
-	$data = include $config_file;
-	if($args){
-		return array_fetch_by_path($data, join('/', $args), null, '/');
-	}
-	return $data;
 }
