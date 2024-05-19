46f878491b29883ae6c76b6dfb17c8313cc51aa6
sasumi <cnsasumi@gmail.com>
2024-05-19 10:28:42 +0800

    upd

--- src/PlsInstaller.php
+++ src/PlsInstaller.php
@@ -2,7 +2,21 @@
 
 namespace LFPhp\Pls;
 
-use Composer\Installer\PluginInstaller;
 
-class PlsInstaller extends PluginInstaller {
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class PlsInstaller implements PluginInterface {
+	public function activate(Composer $composer, IOInterface $io){
+		// TODO: Implement activate() method.
+	}
+
+	public function deactivate(Composer $composer, IOInterface $io){
+		// TODO: Implement deactivate() method.
+	}
+
+	public function uninstall(Composer $composer, IOInterface $io){
+		// TODO: Implement uninstall() method.
+	}
 }
